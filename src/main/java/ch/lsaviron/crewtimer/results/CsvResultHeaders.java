148571728f2ffccab0a0d85890799c2619a3a530
Jean-David Maillefer <jdmaillefer@gmail.com>
2023-10-15 13:49:55 +0200

    documentation code

--- src/main/java/ch/lsaviron/crewtimer/results/CsvResultHeaders.java
+++ src/main/java/ch/lsaviron/crewtimer/results/CsvResultHeaders.java
@@ -2,10 +2,9 @@ package ch.lsaviron.crewtimer.results;
 
 /**
  * List all the result enum fields as return in CrewTimer results CSV. The name
- * must exactly match.
- *
- * @see ch.lsaviron.crewtimer.results.LSM#processResults()
+ * must exactly match the one found in the first row (header).
  */
+// Cf. ch.lsaviron.crewtimer.results.LSM#processResults()
 enum CsvResultHeaders {
 	EventNum,
 	Event,
